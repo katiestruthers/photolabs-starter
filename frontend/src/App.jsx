5c0ce520fbb190601dca83cb5a8bd4ac624df2a0
Katherine Struthers <ktstruthers@gmail.com>
2023-08-25 10:15:26 -0500

    connect favourtie icon and modal view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import photos from './mocks/photos';
 import topics from './mocks/topics';
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import objectToArray from "helpers/objectToArray";
+
 
 const App = () => {
   // Photo details modal state
@@ -12,10 +14,36 @@ const App = () => {
     return setPhoto(currentPhoto);
   }
 
+  // Top nav bar like notification tracker
+  const [favPhoto, setFavPhoto] = useState(false);
+  const notifHandler = () => setFavPhoto(!favPhoto);
+
+  // Create like useStates for each picture
+  for (const key in photos) {
+    const [like, setLike] = useState(false);
+    const likeHandler = () => {
+      notifHandler();
+      return setLike(!like);
+    }
+   photos[key].likedByKey = { like, likeHandler };
+
+    // Set same useStates for similar photos
+    for (const key2 in photos) {
+      const similarPhotosArray = objectToArray(photos[key2].similar_photos);
+
+      for (const photo of similarPhotosArray) {
+        if (Number(photo.id) === Number(key) + 1) {
+          photos[key2].similar_photos[`photo${photo.id}`].likedByKey = { like, likeHandler };
+        }
+      }
+    }
+  }
+
   const homeData = {
     photos: { ...photos },
     topics: { ...topics },
-    photoHandling
+    photoHandling,
+    favPhotoExists: favPhoto
   }
 
   return (
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,36 +2,10 @@ import React, { useState } from 'react';
 import '../styles/HomeRoute.scss';
 import PhotoList from '../components/PhotoList';
 import TopNavigationBar from '../components/TopNavigationBar';
-import objectToArray from "helpers/objectToArray";
 
 const HomeRoute = (props) => {
   const photoData = {photos: {...props.photos}, photoHandling: props.photoHandling};
-  const navData = {topics: {...props.topics}};
-
-  // Notification tracker
-  const [favPhoto, setFavPhoto] = useState(false);
-  const notifHandler = () => setFavPhoto(!favPhoto);
-  navData.favPhotoExists = favPhoto;
-
-  // Create like useStates for each picture
-  for (const key in photoData.photos) {
-    const [like, setLike] = useState(false);
-    const likeHandler = () => {
-      notifHandler();
-      return setLike(!like);
-    }
-    photoData.photos[key].likedByKey = { like, likeHandler };
-
-    // Set same useStates for similar photos
-    for (const key2 in photoData.photos) {
-      const similarPhotosArray = objectToArray(photoData.photos[key2].similar_photos);
-      for (const photo of similarPhotosArray) {
-        if (photo.id === key) {
-          photoData.photos[key].similar_photos[`photo${key}`].likedByKey = { like, likeHandler };
-        }
-      }
-    }
-  }
+  const navData = {topics: {...props.topics}, favPhotoExists: props.favPhotoExists};
 
   return (
     <div className="home-route">
